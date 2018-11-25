bbe6b43e1ab663a1044882b215c5acbd3de1f192
Serpentz <abyicherinet@gmail.com>
2018-11-25 17:55:41 -0500

    heroku 1.0.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+      binding.pry
     if @user.save
       render json: @user, status: :created, location: @user
     else
